d10fda38de19c8280ca81a9a05fb2f304adfa139
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-16 14:19:11 -0400

    reverse working

--- src/com/theironyard/clt/Reverse.java
+++ src/com/theironyard/clt/Reverse.java
@@ -1,10 +1,21 @@
 package com.theironyard.clt;
 
+import java.util.ArrayList;
+
 // Write code to reverse a String. Do not use
 // Any java language features that will perform this action for you.
 public class Reverse {
 
     public static String stringReverse(String string) {
-        string
+        String results;
+        ArrayList<Character> list = new ArrayList<>();
+        char[] work = string.toCharArray();
+
+        for (int i = 0; i < work.length; i++) {
+            list.add(work[work.length - (i + 1)]);
+        }
+        results = list.toString();
+
+        return results;
     }
 }
