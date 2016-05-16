70ba754a86fd1b3240a9f9fce9131423e1cf496a
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-16 15:20:49 -0400

    reverse works

--- src/com/theironyard/clt/Sonny.java
+++ src/com/theironyard/clt/Sonny.java
@@ -11,6 +11,8 @@ public class Sonny extends Robot {
     // Setup Sonny to implement an additional fourth law:
     // "A robot may not harm humanity, or, by inaction, allow humanity to come to harm."
 
+    public static 
+
     // override the base "getLaws" method to print this fourth law.
 
     // add a method called "getName()" that prints "Hello, my name is XXX", where
