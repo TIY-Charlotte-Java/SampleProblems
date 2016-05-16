874bf71d1fab613b5fbec2d832d0e8364f5f8d90
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-16 16:19:02 -0400

    sonny is error

--- src/com/theironyard/clt/Sonny.java
+++ src/com/theironyard/clt/Sonny.java
@@ -15,9 +15,10 @@ public class Sonny extends Robot {
      static ArrayList<String> newLaws = Robot.getLaws();
 
     @Override
-    public static void getLaws() {
+    protected static ArrayList<String> getLaws() {
         newLaws.add("A robot may not harm humanity, or, by inaction, allow humanity to come to harm.");
         System.out.println(newLaws.get(3));
+        return newLaws;
     }
 
     // override the base "getLaws" method to print this fourth law.
@@ -25,8 +26,8 @@ public class Sonny extends Robot {
     // add a method called "getName()" that prints "Hello, my name is XXX", where
     // "XXX" is the name of the current class. In this case, Sonny.
 
-    public static void getName() {
-
+    public void getName() {
+        System.out.println("Hello, my name is Sonny");
     }
     
 }
