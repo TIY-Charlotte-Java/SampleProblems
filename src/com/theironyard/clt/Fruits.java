90381eb1de38f6117e95c4c90b1c856ca6387984
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-24 15:42:45 -0400

    finishing generics problem

--- src/com/theironyard/clt/Fruits.java
+++ src/com/theironyard/clt/Fruits.java
@@ -1,11 +1,20 @@
 package com.theironyard.clt;
 
+import java.util.Scanner;
+
 /**
  * Created by Ben on 5/16/16.
  */
 public class Fruits {
 
     // 1. Read "fruits.csv" from file
+    public void filereading() {
+        Scanner scanner = new Scanner("fruits.csv");
+        while (scanner.hasNext()) {
+
+        }
+
+    }
     // 2. Get all single-word fruits, sort them by length
     // 3. Print the sorted and filtered list.
 }
