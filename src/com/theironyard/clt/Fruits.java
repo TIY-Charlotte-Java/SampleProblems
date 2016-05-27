4a9f3372decbff8fec80f76614ffd77ac78fe31b
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-27 10:51:28 -0400

    working on fruit stand

--- src/com/theironyard/clt/Fruits.java
+++ src/com/theironyard/clt/Fruits.java
@@ -2,7 +2,9 @@ package com.theironyard.clt;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 /**
  * Created by Ben on 5/16/16.
@@ -10,12 +12,15 @@ import java.util.Scanner;
 public class Fruits {
 
     // 1. Read "fruits.csv" from file
-    public void filereading() throws FileNotFoundException{
+    public static Object[] filereading() throws FileNotFoundException{
+        ArrayList<String> fruits = new ArrayList<>();
         File f = new File("fruits.cvs");
         Scanner scanner = new Scanner(f);
         while (scanner.hasNext()) {
-
+            fruits.add(scanner.next());
         }
+
+        return fruits.stream().distinct().filter(Predicate.isEqual("[^\\w]")).toArray();
 
     }
     // 2. Get all single-word fruits, sort them by length
--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,10 +1,14 @@
 package com.theironyard.clt;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
 
         System.out.println(Reverse.stringReverse("bar"));
+
+        System.out.println(Fruits.filereading());
     }
 }
