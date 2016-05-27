0c826b99c4fbe1ebb965d9d94b2737c08d35f5f9
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-05-27 11:36:30 -0400

    close on fruits

--- src/com/theironyard/clt/Fruits.java
+++ src/com/theironyard/clt/Fruits.java
@@ -2,6 +2,7 @@ package com.theironyard.clt;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.function.Predicate;
@@ -14,13 +15,14 @@ public class Fruits {
     // 1. Read "fruits.csv" from file
     public static String fileReading() throws FileNotFoundException{
         ArrayList<String> fruits = new ArrayList<>();
-        File f = new File("fruits");
-        Scanner scanner = new Scanner(f);
+        File f = new File("fruits.csv");
+        FileReader fr = new FileReader(f);
+        Scanner scanner = new Scanner(fr);
         while (scanner.hasNext()) {
             fruits.add(scanner.next());
         }
 
-        return fruits.stream().distinct().filter(Predicate.isEqual("[^\\w]")).toArray().toString();
+        return fruits.stream().distinct().filter(Predicate.isEqual("[^\\w]")).toString();
 
     }
     // 2. Get all single-word fruits, sort them by length
